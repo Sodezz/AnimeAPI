14076dc99681df51b0116b00b6a92cace67ea738
Sodez <korotchenkov245@gmail.com>
2025-08-05 19:16:16 +0300

    - Add description

--- schemas/anime_response.py
+++ schemas/anime_response.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class Anime(BaseModel):
-    russian_anime_title: str = Field(..., examples=["Атака Титанов"])
-    average_score_anime: int = Field(..., examples=[85])
+    russian_anime_title: str = Field(..., examples=["Атака Титанов"], description="Русское название аниме")
+    average_score_anime: int = Field(..., examples=[85], description="Средняя оценка аниме")
